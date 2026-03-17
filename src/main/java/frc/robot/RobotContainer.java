607d6a7a2733a90860a56d9bfbda011301de908a
awfulcarpet <161393599+awfulcarpet@users.noreply.github.com>
2026-03-17 17:46:25 -0600

    implement robot oriented control when holding rightBumper down
    now without formatting the entire file

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -225,7 +225,7 @@ public class RobotContainer {
             driverXbox.start().whileTrue(Commands.none());
             driverXbox.back().whileTrue(Commands.none());
             driverXbox.leftBumper().whileTrue(Commands.runOnce(drivebase::lock, drivebase).repeatedly());
-            driverXbox.rightBumper().onTrue(Commands.none());
+            driverXbox.rightBumper().whileTrue(driveRobotOrientedAngularVelocity);
         }
 
         codriverXbox.leftTrigger()
