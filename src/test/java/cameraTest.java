b75b019266c6af5efe9081366a7b509892f531e4
Maria Nguyen <47044508+Pinkwinx@users.noreply.github.com>
2026-03-18 15:30:49 -0700

    New unit test and also organized pathplanner stuff

---
+++ src/test/java/cameraTest.java
@@ -0,0 +1,34 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+
+import edu.wpi.first.hal.HAL;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import frc.robot.subsystems.IntakeSubsystem;
+import frc.robot.subsystems.DriverCameraSubsystem;
+import java.io.File;
+import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.simulation.DoubleSolenoidSim;
+import edu.wpi.first.wpilibj.simulation.PWMSim;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class cameraTest {
+    IntakeSubsystem intake;
+    DriverCameraSubsystem camera;
+  @BeforeEach // this method will run before each test
+  void setup() {
+    assert HAL.initialize(500, 0); // initialize the HAL, crash if failed
+    camera = new DriverCameraSubsystem();
+
+  }
+
+  @Test // marks this method as a test
+  void poseEstimateTest() {
+    assertNotNull(camera);
+  }
+   
+
+}
